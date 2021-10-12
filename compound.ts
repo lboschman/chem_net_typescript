f59fc9bae7dc64c9693a4050ed8aa703782855b3
Leon Boschman <leon.boschman@gmail.com>
2021-10-12 11:17:30 +0200

    Initial commit.

---
+++ compound.ts
@@ -0,0 +1,9 @@
+export class Compound {
+    name: string; // The name of the compound
+    abundance: number; // the abundance/concentration of the compounds
+
+    constructor(name: string, abundance: number = 0.){
+        this.name = name;
+        this.abundance = abundance;
+    };
+}
