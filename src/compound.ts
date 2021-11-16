10549d33f642fcb5c3e377121be1e2e4be28d5d7
Leon Boschman <leon.boschman@gmail.com>
2021-11-16 21:01:29 +0100

    Added whitespace to conform to code standards

--- src/compound.ts
+++ src/compound.ts
@@ -2,7 +2,7 @@ export class Compound {
     name: string; // The name of the compound
     abundance: number; // the abundance/concentration of the compounds
 
-    constructor(name: string, abundance: number = 0.){
+    constructor(name: string, abundance: number = 0.) {
         this.name = name;
         this.abundance = abundance;
     };
