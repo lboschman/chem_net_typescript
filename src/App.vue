9768cfa7a9d891e4ea348bf63920dfb0c76e519e
Leon Boschman <leon.boschman@gmail.com>
2021-11-18 21:43:11 +0100

    Initial making of new components

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <Navbar/>
   <h1>Hello!!!</h1>
   <HomePage/>
+  <compound-page/>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import HomePage from "./components/HomePage.vue";
 import Navbar from "./components/Menu.vue";
+import CompoundPage from "./components/Compounds.vue";
 
 @Options({
   components: {
     HomePage,
-    Navbar
+    Navbar,
+    CompoundPage
   },
 })
 export default class App extends Vue {}
---
+++ src/components/Compounds.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="compound-list">
+    <h1> Compounds that can react! </h1>
+    <label for="new-compound">Compound name: </label>
+    <input type="text" id="new-compound-name" name="new-compound-name">
+    <input type="button" name="submit-compound" value="Submit!">
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+import { Compound } from "../compound";
+
+export default class CompoundPage extends Vue {
+    compounds: Compound[] = [];
+}
+
+</script>
