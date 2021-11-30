020fba3dfae25026426a2e13a6e73be6edb6cfbc
Leon Boschman <leon.boschman@gmail.com>
2021-11-30 20:16:57 +0100

    Initial Reaction componennt of the app

---
+++ src/components/Reactions.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="reactions"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useStore } from "../store";
+import { Compound } from "../chemical-engine/compound";
+import { Reaction } from "../chemical-engine/reaction";
+
+export default defineComponent({
+  name: "ReactionPage",
+
+  setup() {
+    const store = useStore();
+    {
+      store.state.reactions, store.state.compounds
+    }
+  },
+
+  data() {
+    return {
+      compounds: this.$store.state.compounds as Array<Compound>,
+      reactions: this.$store.state.reactions as Array<Reaction>
+    };
+  }
+
+});
+</script>
+
+<style></style>
